2f944e5f8a208d1a01ff382ce84224c24f4978db
Ayesha Iftikhar <seAyeshaiftikhar@gmail.com>
2021-04-04 15:11:25 +0500

    basics

---
+++ programming basics/file.py
@@ -0,0 +1,51 @@
+# print(100)
+# a = 100 #int  
+# b= 20.6 #float
+# c = "Hello" #string
+# d = 'c' #char 
+# print(a, a + 2)
+
+# = assignment operator: assign values
+#operators:
+# binary operators: at least two operands needed 
+# +,-,*, /, % (remainder operator) #Arithematic operators
+# a = 11
+# comments : non-runable, for programmers help 
+# a operand
+# a == b # expression
+# b = 3 # = right side's value assign to the variable/expression on left side
+#lvalue (koi expression or variable) on left side =(assignment operator) left side
+#rvalue value on right side of = variable, expressions, contants, 
+# lvalues can be used as rvalues
+# rvalues can't be used as lvalues
+# c = a / b 
+# print("Division is")
+# print(c)
+# rational operators
+# >,<, >=, <=, ==, != 
+# logical operators
+# ||(or operator), &&(and operator), >> binary operator
+# ! (not operator) >> unary operator 
+
+# if statement: (only 1 condition)
+# if-else statement (two conditions)
+# if else if statement 
+marks = 90
+# if(marks == 50):
+# 	print('Pass')
+
+# if(marks >= 50):
+# 	print("Pass")
+# else: 
+# 	print("Fail")
+
+# if(marks == 50):
+# 	print("Barely pass")
+# elif(marks > 70):
+# 	print("Grade C")
+# elif(marks > 50):
+# 	print("Pass")
+# else: 
+# 	print("Fail") 
+
+# switch statements: 
